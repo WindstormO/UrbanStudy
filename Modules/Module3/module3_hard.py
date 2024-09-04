a3c1c89aaa8f529e6002019e688baddd755fd7fc
Artyom Pershin <artempershin@inbox.ru>
2024-09-04 21:52:47 +0300

    Update module3_hard.py
    Изменена структура оболочки

--- Modules/Module3/module3_hard.py
+++ Modules/Module3/module3_hard.py
@@ -1,25 +1,31 @@
-def calculate_structure_sum(data_structure):
-    sum = 0
-    for item in range(len(data_structure)):
-        print(type(data_structure[item]))
-        if isinstance(data_structure[item], int):
-            sum = sum + data_structure[item]
-        elif isinstance(data_structure[item], float):
-            sum = sum + data_structure[item]
-        elif isinstance(data_structure[item], str):
-            #for symbol in (data_structure[item]):
-            #не понятно, что делать с текстовыми строками
-            print("СТРОКА")
-        elif isinstance(data_structure[item], list):
-            for list_item in range(len(data_structure[item])):
-                sum = sum + data_structure[item][list_item]
-            print("СПИСОК")
-        elif isinstance(data_structure[item], dict):
-            for dict_item in data_structure[item]:
-                sum = sum+data_structure[item][dict_item]
-            print("СЛОВАРЬ")
-        elif isinstance(data_structure[item], tuple):
-            print("КОРТЕЖ")
+def calculate_structure_sum(*args):
+    total_sum = 0
+    for arg in args:
+        if isinstance(arg, list,tuple,set):
+            print("LISTTUPLESET")        
+        elif isinstance(arg, dict):
+            print("DICT")
+        elif isinstance(arg, str):
+            total_sum += len(arg)
+        elif isinstance(arg, int, float):
+            total_sum += arg
+        elif isinstance(arg, none):
+            pass
+    return total_sum
+
+
+#входные данные
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+#на выходе должны получить 99
+result = calculate_structure_sum(data_structure)
+print(result)
     return sum
 
 
