9ed6c4ab8d517f060f7ee3098f074a8186288343
Artyom Pershin <artempershin@inbox.ru>
2024-09-04 22:48:28 +0300

    Update module3_hard.py
    Отредактирована оболочка. Ошибка на 9 строке

--- Modules/Module3/module3_hard.py
+++ Modules/Module3/module3_hard.py
@@ -1,31 +1,22 @@
 def calculate_structure_sum(*args):
     total_sum = 0
     for arg in args:
-        if isinstance(arg, list,tuple,set):
-            print("LISTTUPLESET")        
+        if isinstance(arg, set):
+            total_sum += calculate_structure_sum(*arg)
+        elif isinstance(arg, tuple):
+            total_sum += calculate_structure_sum(*arg)
+        elif isinstance(arg, list):
+            total_sum += int(temp)
         elif isinstance(arg, dict):
-            print("DICT")
+            total_sum += calculate_structure_sum(*arg.items())
         elif isinstance(arg, str):
             total_sum += len(arg)
-        elif isinstance(arg, int, float):
+        elif isinstance(arg, float):
+            total_sum += arg
+        elif isinstance(arg, int):
             total_sum += arg
         elif isinstance(arg, none):
             pass
-    return total_sum
-
-
-#входные данные
-data_structure = [
-[1, 2, 3],
-{'a': 4, 'b': 5},
-(6, {'cube': 7, 'drum': 8}),
-"Hello",
-((), [{(2, 'Urban', ('Urban2', 35))}])
-]
-
-#на выходе должны получить 99
-result = calculate_structure_sum(data_structure)
-print(result)
     return sum
 
 
