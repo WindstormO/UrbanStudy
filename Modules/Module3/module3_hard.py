e2e81243f0cf910eadfb5d32663847f1f6fda5cb
Artyom Pershin <artempershin@inbox.ru>
2024-09-04 12:58:37 +0300

    Update module3_hard.py
    Создана основа, добавлен вывод данных и прописаны входные данные. Осталось только написать саму функцию...

--- Modules/Module3/module3_hard.py
+++ Modules/Module3/module3_hard.py
@@ -1,1 +1,16 @@
+def calculate_structure_sum(data_structure):
+    
+    
 
+#входные данные
+data_structure = [
+[1, 2, 3],
+{'a': 4, 'b': 5},
+(6, {'cube': 7, 'drum': 8}),
+"Hello",
+((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+#на выходе должны получить 99
+result = calculate_structure_sum(data_structure)
+print(result)
