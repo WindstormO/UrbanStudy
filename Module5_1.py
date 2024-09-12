643f4ec41350a09e70df9bf84ee94227a1b22f0d
Artyom Pershin <artempershin@inbox.ru>
2024-09-12 23:51:25 +0300

    Update Module5_1.py
    Исправлены все ошибки

--- Module5_1.py
+++ Module5_1.py
@@ -4,15 +4,15 @@ class House:
         self.number_of_floors = number_of_floors
         
         
-    def go_to(new_floor):
+    def go_to(self, new_floor):
         if new_floor > self.number_of_floors:
             print("нет такого этажа")
         else:
-            for floor in new_floor:
-                print(floor)
+            for floor in range(new_floor):
+                print(floor+1)
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
-#h2.go_to(10)                
+h2.go_to(10)                
