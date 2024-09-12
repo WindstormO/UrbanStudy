a8880cf3f78e53c7f9f167b6bccd3a586c142831
Artyom Pershin <artempershin@inbox.ru>
2024-09-13 01:06:51 +0300

    Update Module5_3.py
    Что-то добавил, что-то нет

--- Module5_3.py
+++ Module5_3.py
@@ -18,15 +18,71 @@ class House:
         
     def __str__(self):
         return (f"Название: {self.name}, этажи: {self.number_of_floors}")
+        
+    def __eq__(self, other):
+        if self.number_of_floors == other.number_of_floors:
+            return True
+            
+    
+    def __lt__(self, other):
+        if self.number_of_floors < other.number_of_floors:
+            return True
+            
+    
+    def __le__(self, other):
+        if self.number_of_floors <= other.number_of_floors:
+            return True
+            
+    def __gt__(self, other):
+        if self.number_of_floors > other.number_of_floors:
+            return True        
 
 
+    def __ge__(self, other):
+        if self.number_of_floors >= other.number_of_floors:
+            return True
+            
+            
+    def __ne__(self, other):
+        if self.number_of_floors != other.number_of_floors:
+            return True    
+
+     
+    def __add__(self, value):
+        self.number_of_floors+=value
+        return self
+        
+            
+    def __radd__(self, value):
+        self.number_of_floors+=value
+        return self
+            
+            
+    def __iadd__(self, value):
+        self.number_of_floors+=value
+        return self
+        
+            
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-               
-# __str__
+
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
