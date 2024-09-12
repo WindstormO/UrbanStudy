810e10b5aca74176a48e3ba7edc67b8bc5894d22
Artyom Pershin <artempershin@inbox.ru>
2024-09-12 23:43:57 +0300

    Update Module5_1.py
    Вроде бы всё прописано, но выдаёт ошибку на 17 строке (при вызове h1.go_to(5) "go_to requires 1 positional argument but takes 2 were given"

--- Module5_1.py
+++ Module5_1.py
@@ -1,12 +1,18 @@
-class House(self, name, number_of_floors):
-    def __init__(self):
+class House:
+    def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
         
+        
     def go_to(new_floor):
         if new_floor > self.number_of_floors:
-            print(f"Этажа {new_floor} не существует")
+            print("нет такого этажа")
         else:
             for floor in new_floor:
                 print(floor)
-              
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+#h2.go_to(10)                
