6913a01c71c867a8e84a1b9c131a5ce29e2d7b17
Artyom Pershin <artempershin@inbox.ru>
2024-09-12 23:03:39 +0300

    Update Module5_1.py
    Прописан класс House, метод go_to. Их основная логика

--- Module5_1.py
+++ Module5_1.py
@@ -1,1 +1,12 @@
-
+class House(self, name, number_of_floors):
+    def __init__(self):
+        self.name = name
+        self.number_of_floors = number_of_floors
+        
+    def go_to(new_floor):
+        if new_floor > self.number_of_floors:
+            print(f"Этажа {new_floor} не существует")
+        else:
+            for floor in new_floor:
+                print(floor)
+              
